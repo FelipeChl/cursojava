303ab4889dcc482a2866073bbc76f23953c7db0c
Felipe Chaves <felipechavespsp@gmail.com>
2025-05-12 20:52:30 -0300

    estudo sobre saida de dados

---
+++ saidadedados/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Olá, mundo"); // primeiro olá mundo em java
+
+        System.out.println("Boa noite"); //ln no print quebra linha
+
+        int y = 32; //Declaração de uma variavel do tipo inteiro recebendo valor fixo.
+        double x = 10.5555;
+
+        System.out.println(y); //Print da variavel int
+        System.out.println(x); //Print da variavel double
+        System.out.printf("%.2f%n", x); //Print da variavel double com somente duas casas decimais após a virgula com quebra de linha
+
+    }
+}
